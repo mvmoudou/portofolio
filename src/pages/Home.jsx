453cb0bf5387d6b02312f6959f5eac14ab51e015
SIDIBE Mamoudou <120347148+Moudou3@users.noreply.github.com>
2026-06-06 13:30:50 +0200

    Mon Portofolio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import Navbar from '../components/portfolio/Navbar';
+import HeroSection from '../components/portfolio/HeroSection';
+import AboutSection from '../components/portfolio/AboutSection';
+import SkillsSection from '../components/portfolio/SkillsSection';
+import ProjectsSection from '../components/portfolio/ProjectsSection';
+import EducationSection from '../components/portfolio/EducationSection';
+import ContactSection from '../components/portfolio/ContactSection';
+import Footer from '../components/portfolio/Footer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <EducationSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+}
